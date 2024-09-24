8a40349e964d17370469d08ff08c804de4b9bce1
fatecjundiai <cd.fatecjundiai@gmail.com>
2024-09-24 18:46:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+nav.setAttribute("data-visible", false);
+
+navToggle.addEventListener("click", () =>{
+    const visibility = nav.getAttribute("data-visible");
+    
+    if(visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        nav.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        nav.setAttribute("aria-expanded", false);
+    }
+
+    console.log(nav.getAttribute('aria-expanded'));
+})
